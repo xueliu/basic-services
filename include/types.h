2bd5160b33833e777ce3e4985a9d5ade0b2ca6f3
Xue Liu <liuxuenetmail@gmail.com>
2021-01-16 00:40:01 +0100

    - init type header file

---
+++ include/types.h
@@ -0,0 +1,15 @@
+//
+// Created by clock on 16/01/2021.
+//
+
+#ifndef BASIC_SERVICES_TYPES_H
+#define BASIC_SERVICES_TYPES_H
+
+#include <cstdint>
+#include <string>
+
+#ifndef NDEBUG
+#include <assert.h>
+#endif
+
+#endif //BASIC_SERVICES_TYPES_H
