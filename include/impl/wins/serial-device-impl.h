1f34eae87e45c771d4cac195407e021bfa3c7131
Xue Liu <liuxuenetmail@gmail.com>
2021-01-23 23:35:49 +0100

    - init serial device windows implementation

--- include/impl/wins/serial-device-impl.h
+++ include/impl/wins/serial-device-impl.h
@@ -5,6 +5,10 @@
 #ifndef BASIC_SERVICES_SERIAL_DEVICE_WINS_IMPL_H
 #define BASIC_SERVICES_SERIAL_DEVICE_WINS_IMPL_H
 
+#include "serial-device.h"
+
+namespace basic {
+
 class BASIC_SERVICES_NO_EXPORT SerialDevice::Impl {
 
 public:
@@ -13,7 +17,7 @@ public:
 	~Impl();
 
 public:
-	bool IsValid() const noexcept { return fd_ >= 0; }
+	bool IsValid() const noexcept { return true; }
 
 	size_t ReadSome(uint8_t * data, size_t size);
 
@@ -25,5 +29,7 @@ public:
 
 	ModemLine SetLine(ModemLine mask, ModemLine value);
 };
+
+} // namespace basic
 
 #endif //BASIC_SERVICES_SERIAL_DEVICE_WINS_IMPL_H
--- src/serial-device-wins.cpp
+++ src/serial-device-wins.cpp
@@ -2,3 +2,38 @@
 // Created by clock on 23/01/2021.
 //
 
+#include "serial-device.h"
+#include "serial-device-impl.h"
+
+namespace basic {
+
+SerialDevice::Impl::Impl(char const *name, SerialDevice::Configuration const &config) {
+
+}
+
+SerialDevice::Impl::~Impl() {
+
+}
+
+size_t SerialDevice::Impl::ReadSome(uint8_t *data, size_t size) {
+	return 0;
+}
+
+size_t SerialDevice::Impl::WriteSome(uint8_t const *data, size_t size) {
+	return 0;
+}
+
+void SerialDevice::Impl::Close() {
+
+}
+
+SerialDevice::ModemLine SerialDevice::Impl::GetLine() const {
+
+}
+
+SerialDevice::ModemLine SerialDevice::Impl::SetLine(ModemLine mask, ModemLine value) {
+
+}
+
+
+} // namespace basic
