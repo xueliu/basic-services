e40f763a6d6bb7995196025eb5ca2d360ac69947
Xue Liu <liuxuenetmail@gmail.com>
2021-01-31 23:49:00 +0100

    - add example for class Logger

--- examples/ex-log.cpp
+++ examples/ex-log.cpp
@@ -2,3 +2,16 @@
 // Created by clock on 26/01/2021.
 //
 
+#include "logging.h"
+
+auto main() -> int
+{
+	LOG_TRACE << "This is a trace message";
+	LOG_DEBUG << "This is a debug message";
+	LOG_INFO << "This a a info message";
+	LOG_WARN << "This a warning message";
+	LOG_ERROR << "This is en error message!";
+	LOG_SYSERR << "This is a system error message!";
+
+	return 0;
+}
